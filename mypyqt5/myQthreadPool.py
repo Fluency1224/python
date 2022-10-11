ab29c0f5ea023efa4fc81b5759ae2737fc4470de
Bxl <1005068694@qq.com>
2022-10-11 18:04:41 +0800

    add Qthread demo

---
+++ mypyqt5/myQthreadPool.py
@@ -0,0 +1,64 @@
+import logging
+import random
+import sys
+import time
+
+from PyQt5.QtCore import QRunnable, Qt, QThreadPool
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+# 1. Subclass QRunnable
+class Runnable(QRunnable):
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+
+    def run(self):
+        # Your long-running task goes here ...
+        for i in range(5):
+            logging.info(f"Working in thread {self.n}, step {i + 1}/5")
+            time.sleep(random.randint(700, 2500) / 1000)
+
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle("QThreadPool + QRunnable")
+        self.resize(250, 150)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+        # Create and connect widgets
+        self.label = QLabel("Hello, World!")
+        self.label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        countBtn = QPushButton("Click me!")
+        countBtn.clicked.connect(self.runTasks)
+        # Set the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(countBtn)
+        self.centralWidget.setLayout(layout)
+
+    def runTasks(self):
+        threadCount = QThreadPool.globalInstance().maxThreadCount()
+        self.label.setText(f"Running {threadCount} Threads")
+        pool = QThreadPool.globalInstance()
+        for i in range(threadCount):
+            # 2. Instantiate the subclass of QRunnable
+            runnable = Runnable(i)
+            # 3. Call start()
+            pool.start(runnable)
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
